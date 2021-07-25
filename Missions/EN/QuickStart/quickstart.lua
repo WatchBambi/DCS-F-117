72ea6e89baa09e9b65ca45400ef02373f01ed567
Vincent <vincentwingelaar@gmail.com>
2021-07-25 16:36:50 +0200

    Quick Start Missions
    Added very basic quick start missions for: Nevada/Caucasus/Marinas

---
+++ Missions/EN/QuickStart/quickstart.lua
@@ -0,0 +1,18 @@
+planes = {
+-- Caucasus
+    { 
+    name = _('F-117 Hot Parking Kobuleti'),
+    file = 'QuickFlightf117_Caucasus_Kobuleti.miz',
+    },
+-- Nevada
+    { 
+    name = _('F-117 Hot Parking Tonopah'),
+    file = 'QuickFlightf117_Nevada_Tonopah.miz',
+    },
+-- Marinas
+    { 
+    name = _('F-117 Hot Parking Andersen AFB'),
+    file = 'QuickFlightf117_Marinas_Andersen.miz',
+    },
+}
+	 
