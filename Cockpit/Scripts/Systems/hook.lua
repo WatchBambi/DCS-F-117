3ba6d0a801e5d9cf2fcbcfa75ce3f1351e83a3ed
Vincent <vincentwingelaar@gmail.com>
2021-07-28 00:25:01 +0200

    hook fix?

--- Cockpit/Scripts/Systems/hook.lua
+++ Cockpit/Scripts/Systems/hook.lua
@@ -7,6 +7,8 @@ make_default_activity(update_time_step)
 
 local sensor_data = get_base_data()
 
+local HOOK = get_param_handle("HOOK")
+
 local hook = 0
 
 dev:listen_command(69) -- Hook command
@@ -22,5 +24,15 @@ function SetCommand(command,value)
 end
 
 function update()
-    set_aircraft_draw_argument_value(25,hook)
+    local HookStatus = HOOK:get()
+
+    if (hook == 0 and HookStatus > 0) then
+        HookStatus = HookStatus - 0.01
+        set_aircraft_draw_argument_value(25,HookStatus)
+        HOOK:set(HookStatus)
+    elseif (hook == 1 and HookStatus < 1) then
+        HookStatus = HookStatus + 0.01
+        set_aircraft_draw_argument_value(25,HookStatus)
+        HOOK:set(HookStatus)
+    end
 end
