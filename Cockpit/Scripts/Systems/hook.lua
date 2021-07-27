e6f0bf1d57bd181a4ee9d4bea72ee18e409f0c79
Vincent <vincentwingelaar@gmail.com>
2021-07-28 00:44:28 +0200

    fix hook

--- Cockpit/Scripts/Systems/hook.lua
+++ Cockpit/Scripts/Systems/hook.lua
@@ -19,7 +19,7 @@ end
 
 function SetCommand(command,value)
     if command == 69 then
-        hook = 1 - hook
+        hook = 0.9 - hook
     end
 end
 
@@ -30,7 +30,7 @@ function update()
         HookStatus = HookStatus - 0.01
         set_aircraft_draw_argument_value(25,HookStatus)
         HOOK:set(HookStatus)
-    elseif (hook == 1 and HookStatus < 1) then
+    elseif (hook == 0.9 and HookStatus < 0.9) then
         HookStatus = HookStatus + 0.01
         set_aircraft_draw_argument_value(25,HookStatus)
         HOOK:set(HookStatus)
